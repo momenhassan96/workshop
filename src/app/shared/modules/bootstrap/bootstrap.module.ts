7c24081b9f4b5eda62d631c7c1be193a3f38c8e6
momenhassan96 <momenhassan1996@gmail.com>
2020-11-18 11:51:24 +0200

    workshop-3: add carousle

--- src/app/shared/modules/bootstrap/bootstrap.module.ts
+++ src/app/shared/modules/bootstrap/bootstrap.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgbModule,NgbAlert } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule, NgbAlert, NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    NgbModule
-  ],
-  exports:[
     NgbModule,
-    NgbAlert
+    // NgbCarousel,
+  ],
+  exports: [
+    NgbModule,
+    // NgbAlert,
+    // NgbCarousel
   ]
 })
 export class BootstrapModule { }
